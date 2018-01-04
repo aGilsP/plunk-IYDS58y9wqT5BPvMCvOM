3c86a5093638eb3cbd8a06365c7623725ddac6fd
aGilsP <gilles_andre@yahoo.com>
2018-01-04 23:01:13 +0100

    listening to different port

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ var config = {headers: {
 };
 App.controller('choristes_ctrl', function ($scope, $http) {
   //$http.get('todos.json', config)
-  $http.get('http://127.0.0.1:8080/camt/w/choristes', {
-    'Content-Type': 'text/html',
+//  $http.get('http://127.0.0.1:8080/camt/w/choristes', {
+  $http.get('http://127.0.0.1:1336/choristes', {
+    'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE'
   })
